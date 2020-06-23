4251c725663437763d1e05e374fb057dfba17c15
Dan Wong <dawong@twilio.com>
2020-06-23 16:04:27 +1000

    working version of websocket

--- connector/app.js
+++ connector/app.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var recordRouter = require('./routes/ws-record');
+const debug = require('debug')('connector:app');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const recordRouter = require('./routes/ws-record');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,5 +20,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/record', recordRouter); 
+
+// Specific route handling for setting up websocket connection
+app.handleUpgrade = function(pathname, request, socket, head) {
+  if (pathname.replace(/\/$/, '') === '/record') {
+    debug('Upgrading socket for path ' + pathname);
+    recordRouter.wss.handleUpgrade(request, socket, head, (ws) => {
+      debug('Emitting connection for paht ' + pathname)
+      recordRouter.wss.emit('connection', ws, request);
+    });
+  } else {
+    debug('unhandlled path: ' + pathname);
+    socket.destroy();
+  }
+}
 
 module.exports = app;
--- connector/routes/ws-record.js
+++ connector/routes/ws-record.js
@@ -1,24 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const debug = require('debug')('connector:routes:ws-record');
 
-router.use('/', function(req, res) {
+// Set up the WebSocket for the route
+const WebSocket = require('ws')
+const wss = new WebSocket.Server({ noServer: true });
+router.wss = wss;
+
+wss.on('connection', (ws) => {
+  debug(`Connection Received. Number of clients = ${wss.clients.size}`);
+  
+  ws.on('message', (message) => {
+      debug('received: %s', message);
+  });
+
+  ws.on('close', () => {
+    debug(`Connection Closed. Number of clients = ${wss.clients.size}`);
+  });
+
+  ws.send(JSON.stringify('Connection /record Opened'));
+})
+
+// normal HTTP route
+router.get('/', function(req, res) {
 
   //let ws = req.ws
-  console.log('called ws')
-  let wss = req.app.wss
-  wss.on('connection', (ws) => {
-    console.log(`Connection Received. Number of clients = ${wss.clients.size}`);
-    ws.on('message', (message) => {
-        console.log('received: %s', message);
-    });
-
-    ws.on('close', () => {
-      console.log(`Connection Closed. Number of clients = ${wss.clients.size}`);
-    });
-
-    ws.send(JSON.stringify('Connection Opened'));
-  });
-  res.send('Socket connected');
+  debug('GET called')
+  res.status(200).send({ status: 'Ok', message: 'Executed Action'});
 });
 
 module.exports = router;
