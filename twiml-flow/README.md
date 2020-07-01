4f40600f7d0ba172668ba02cbb31b6c7541c3b45
Dan Wong <dawong@twilio.com>
2020-07-01 14:03:56 +1000

    expanded functionality for gather. Read README o see enhancements

--- twiml-flow/README.md
+++ twiml-flow/README.md
@@ -5,6 +5,27 @@ The Flow Definition is stored under `studio/` directory. Please use [twilio stud
 
 Currently flow's connector endpoint is pointing to my localhost hosted connector. Please repoint this if you're using in your own environment. Replace `dawong.au.ngrok.io` with your server. This is your `CONNECTOR_SERVER`
 
+### Gather Options
+| Parameters | Description |
+| :--| :-- |
+| FlowSid    | (Required) FlowSID passed from Studio. This is required to be explicitly passed |
+| Count      | (Required) Sequence Count of the Question as part of the enrolment |
+| maxdigits  | (Optional) Maximum Length of string for enrolment. (default: 8)|
+| enrolType | (Optional) Enrolment Question Type, if not defined it will take `EnrolTypeOrder` defined in [enrolment_gather.js](./functions/enrolment/enrolment_gather.js) Refer to table below for valid Enrol Type|
+| NoRepeat | (Optional) Boolean value defining if Repeated Digits are allowed. |
+| NoZeros | (Optional) Boolean value defining if Zero (0) is allowed. |
+
+The following Enrolment Type will take into account `maxdigits`, `NoRepeat`, `NoZeros`
+| Enrol Type | Description |
+| :-- | :-- |
+| `Alphanumeric`| Alphanumeric String `[0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ]` |
+| `Digits`| Numeric String `[0123456789]` |
+| `Caller`| Caller's Phone Number as defined in `Caller` field from Twilio |
+| `CountUp`| Counting Up from 1 to 9 |
+| `CountDown`| Counting Down from 9 to 1 |
+| `FourTwo`| Repeating 4 Digit String for example `9657 9657`|
+
+
 ### Environment Variables
 
 | Config Value | Description |
--- twiml-flow/functions/enrolment/enrolment_gather.js
+++ twiml-flow/functions/enrolment/enrolment_gather.js
@@ -7,13 +7,76 @@ const twilio_version = require('twilio/package.json').version;
 const RETURN_WEBHOOK = 'https://webhooks.twilio.com/v1/Accounts/AC72c69ce8a6745ba82d93ba0b2c94cbfa/Flows/FW26c0e6ad9d690eef1c7a69f2d116663c?FlowEvent=return'
 const LANG = 'en-AU'
 const VOICE = 'Polly.Russell'
-const ALPHANUMERIC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+const ALPHANUMERIC = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-const ET_ALPHANUM = 'Alphanumeric'
-const ET_DIGITS = 'Digits'
+const ET_ALPHANUM = 'Alphanumeric' // Use Alpha Numeric String of x Length
+const ET_DIGITS = 'Digits'         // Use Random Digits of x Length
+const ET_CALLER = 'Caller'         // Use Caller Phone Number
+const ET_COUNTUP = 'CountUp'       // Count up from 'one to nine'
+const ET_COUNTDOWN = 'CountDown'   // Count down from 'nine to one'
+const ET_FOURTWO = 'FourTwo'       // Repeated 4 Digit String
+
+const EnrolTypeOrder = [
+  ET_CALLER, 
+  ET_CALLER, 
+  ET_COUNTUP, 
+  ET_COUNTDOWN,
+  ET_FOURTWO
+];
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getPhrase(opts) {
+
+  let digitArr = [];
+
+  if(opts.enrolType === ET_CALLER )    { return { phrase: 'Please say the phone number you are calling from', literal: opts.caller.replace(/\+/, '').replace(/^61/, '0')}};
+  if(opts.enrolType === ET_COUNTUP )   { return { phrase: 'Please count up from one to nine', literal: '123456789'}};
+  if(opts.enrolType === ET_COUNTDOWN ) { return { phrase: 'Please count down from nine to one', literal: '987654321'}};
+
+  let selectionString = ALPHANUMERIC;
+  let phraseLength = (opts.enrolType === ET_FOURTWO) ? 4 : ((opts.maxdigits) ? opts.maxdigits : 8);
+  if( opts.enrolType === ET_DIGITS || opts.enrolType === ET_FOURTWO ) selectionString = ALPHANUMERIC.replace(/[A-Z]*/g, '');
+  if( opts.noZeros ) selectionString = selectionString.replace(/0/g, '');
+
+  console.log(`Selection from ${selectionString}`);
+
+  for( let i = 0; i < phraseLength; i++) {
+
+    const randInt = getRandomInt(selectionString.length);
+    const selected = selectionString.substring(randInt, randInt + 1)
+    digitArr.push(selected);
+    if( opts.noRepeat ) {
+      selectionString = selectionString.replace(selected, '');
+      console.log(`Selection from ${selectionString}`);
+    }
+  } 
+
+  if(opts.enrolType === ET_FOURTWO) { digitArr = digitArr.concat(digitArr) }
+  
+ return { phrase: digitArr.join(''), literal: digitArr.join('') };
+
+}
+
+function buildSay(sayElem, opts) {
+
+  switch( opts.enrolType ) {
+    case ET_CALLER:
+    case ET_COUNTUP:
+    case ET_COUNTDOWN:
+      sayElem.addText(opts.phrase);
+      break;
+    default:
+      sayElem.addText('Please Say');
+      sayElem.prosody({
+        rate: '75%',
+      }, '').addChild('say-as', {
+        'interpret-as': 'spell-out'
+      }).addText(opts.phrase);
+  }
+  return sayElem;
 }
 
 exports.handler = function(context, event, callback) {
@@ -24,41 +87,33 @@ exports.handler = function(context, event, callback) {
     console.log('Event ' + JSON.stringify(event));
   }
   let twiml = new Twilio.twiml.VoiceResponse();
-
-  let digitArr = [];
   
   // https://cloud.google.com/speech-to-text/docs/class-tokens
   let recHints = (event.enrolType === ET_ALPHANUM) ? '$OOV_CLASS_ALPHANUMERIC_SEQUENCE' : '$OOV_CLASS_DIGIT_SEQUENCE'
-  
-  for( let i = 0; i < event.maxdigits; i++) {
-     if( event.enrolType === ET_ALPHANUM) {
-        const randInt = getRandomInt(ALPHANUMERIC.length)
-        digitArr.push(ALPHANUMERIC.substring(randInt, randInt + 1));
-     } else {
-        digitArr.push(Math.floor(Math.random() * Math.floor(9)));
-     }
-  }
-  
-  const enrol_code = digitArr.join('');
-  console.log('Enrol Code ' + enrol_code.split('').join(' '))
 
   const connector_uri = `wss://${context.CONNECTOR_SERVER}/record`
   
   // moved this to before start stream so the speech start is closer to <gather>
-
   const say = twiml.say({
     voice: VOICE,
     language: LANG
-  } ,'Please say ');
-  
-  const prosody = say.prosody({
-     rate: '75%',
-  }, '');
-
-  const sayAs = prosody.addChild('say-as', {
-    'interpret-as': 'spell-out'
   })
-  sayAs.addText(enrol_code);
+  const enrolType = (event.enrolType) ? event.enrolType : EnrolTypeOrder[event.Count];
+
+  const { phrase, literal } = getPhrase({
+    enrolType: enrolType,
+    maxdigits: event.maxdigits,
+    caller: event.Caller,
+    noRepeat: (event.NoRepeat === 'true') ? true : false,
+    noZeros: (event.NoZeros === 'true') ? true : false
+  });
+
+  console.log('Literal will be ' + literal.split('').join(' '))
+
+  buildSay(say, { 
+    enrolType: enrolType, 
+    phrase: phrase
+  });
   
   const start = twiml.start(); 
   const stream = start.stream({
@@ -66,17 +121,27 @@ exports.handler = function(context, event, callback) {
       url: connector_uri,
       track: 'inbound_track'
   })
+  // to be deprecated
   stream.parameter({
       name: 'enrolCode',
-      value: enrol_code
+      value: literal
   });
   stream.parameter({
       name: 'caller',
       value: event.Caller
   });
+
   stream.parameter({
-      name: 'question',
-      value: event.question
+      name: 'literal',
+      value: literal
+  });
+  stream.parameter({
+      name: 'sequence',
+      value: event.Count
+  });
+  stream.parameter({
+      name: 'type',
+      value: enrolType
   });
   stream.parameter({
       name: 'mode',
@@ -94,6 +159,7 @@ exports.handler = function(context, event, callback) {
   });
   
   twiml.say('We didn\'t receive any input. Goodbye!');
+  console.log(twiml.toString());
  
   callback(null, twiml)
 };
