e5ad7cddf4993888f6394e89f6f2c9ee06ef406d
Dan Wong <dawong@twilio.com>
2020-07-02 16:00:14 +1000

    return error message if speechResult does not match literal

--- connector/routes/recresult.js
+++ connector/routes/recresult.js
@@ -7,31 +7,31 @@ const debug = require('debug')('connector:routes:recresult');
 
 function requestPrint(req) {
   debug('---------------------------------------------------------------------');
-  debug('req._startTime       ' + JSON.stringify(req._startTime       , null, 2));
-  debug('req.body             ' + JSON.stringify(req.body             , null, 2));
-  debug('req.cookies          ' + JSON.stringify(req.cookies          , null, 2));
-  debug('req.files            ' + JSON.stringify(req.files            , null, 2));
-  debug('req.headers          ' + JSON.stringify(req.headers          , null, 2));
-  debug('req.httpVersion      ' + JSON.stringify(req.httpVersion      , null, 2));
-  debug('req.httpVersionMajor ' + JSON.stringify(req.httpVersionMajor , null, 2));
-  debug('req.httpVersionMinor ' + JSON.stringify(req.httpVersionMinor , null, 2));
-  debug('req.protocol         ' + JSON.stringify(req.protocol         , null, 2));
-  debug('req.method           ' + JSON.stringify(req.method           , null, 2));
-  debug('req.baseUrl          ' + JSON.stringify(req.baseUrl          , null, 2));
-  debug('req.originalUrl      ' + JSON.stringify(req.originalUrl      , null, 2));
-  debug('req.path             ' + JSON.stringify(req.path             , null, 2));
-  debug('req.hostname         ' + JSON.stringify(req.hostname         , null, 2));
-  debug('req.subdomain        ' + JSON.stringify(req.subdomain        , null, 2));
-  debug('req.ip               ' + JSON.stringify(req.ip               , null, 2));
-  debug('req.params           ' + JSON.stringify(req.params           , null, 2));
-  debug('req.query            ' + JSON.stringify(req.query            , null, 2));
-  debug('req.readable         ' + JSON.stringify(req.readable         , null, 2));
-  debug('req.signedCookies    ' + JSON.stringify(req.signedCookies    , null, 2));
-  debug('req.url              ' + JSON.stringify(req.url              , null, 2));
+  debug('req._startTime       ' + JSON.stringify(req._startTime, null, 2));
+  debug('req.body             ' + JSON.stringify(req.body, null, 2));
+  debug('req.cookies          ' + JSON.stringify(req.cookies, null, 2));
+  debug('req.files            ' + JSON.stringify(req.files, null, 2));
+  debug('req.headers          ' + JSON.stringify(req.headers, null, 2));
+  debug('req.httpVersion      ' + JSON.stringify(req.httpVersion, null, 2));
+  debug('req.httpVersionMajor ' + JSON.stringify(req.httpVersionMajor, null, 2));
+  debug('req.httpVersionMinor ' + JSON.stringify(req.httpVersionMinor, null, 2));
+  debug('req.protocol         ' + JSON.stringify(req.protocol, null, 2));
+  debug('req.method           ' + JSON.stringify(req.method, null, 2));
+  debug('req.baseUrl          ' + JSON.stringify(req.baseUrl, null, 2));
+  debug('req.originalUrl      ' + JSON.stringify(req.originalUrl, null, 2));
+  debug('req.path             ' + JSON.stringify(req.path, null, 2));
+  debug('req.hostname         ' + JSON.stringify(req.hostname, null, 2));
+  debug('req.subdomain        ' + JSON.stringify(req.subdomain, null, 2));
+  debug('req.ip               ' + JSON.stringify(req.ip, null, 2));
+  debug('req.params           ' + JSON.stringify(req.params, null, 2));
+  debug('req.query            ' + JSON.stringify(req.query, null, 2));
+  debug('req.readable         ' + JSON.stringify(req.readable, null, 2));
+  debug('req.signedCookies    ' + JSON.stringify(req.signedCookies, null, 2));
+  debug('req.url              ' + JSON.stringify(req.url, null, 2));
   debug('---------------------------------------------------------------------');
 }
 
-router.post('/', (req, res, next)=>{
+router.post('/', (req, res, next) => {
   requestPrint(req);
 
   let i = 0;
@@ -43,7 +43,7 @@ router.post('/', (req, res, next)=>{
     // }
 
     const metafile = `media/${req.body.CallSid}-${req.body.Count}-${mode}.json`
-    if( fs.existsSync(metafile)) {
+    if (fs.existsSync(metafile)) {
       debug(`found ${metafile}`)
       let data = JSON.parse(fs.readFileSync(metafile));
       debug(`${JSON.stringify(data, null, 2)}`)
@@ -55,24 +55,54 @@ router.post('/', (req, res, next)=>{
 
       debug(`writing back to ${metafile}`);
       debug(`${JSON.stringify(data, null, 2)}`)
+
       fs.writeFile(metafile, JSON.stringify(data, null, 2), (err) => {
-        if(err) {
+        if (err) {
           debug(`error writing file: ${metafile}: ${err}`);
-          res.status(500).send({status: 'error', message: 'Write error: ' + err})
+          res.status(500).send({
+            status: 'error',
+            message: 'Write error: ' + err
+          })
 
         }
         debug(`Result written into ${metafile}`);
-        res.status(200).send({status: 'ok', message: 'Result written to file', streamSid: data.streamSid })
+
+        // check data.customParameters.literal === data.recognitionResult.speechResult
+        if (data.customParameters.literal !== data.recognitionResult.speechResult) {
+          debug(`SpeechResult does not match Literal ${data.customParameters.literal} !== ${data.recognitionResult.speechResult}`);
+          res.status(200).send({
+            status: 'error',
+            message: 'SpeechResult does not match Literal.',
+            data: {
+              expected: data.customParameters.literal,
+              speechresult: data.recognitionResult.speechResult
+            }
+          });
+          return;
+        }
+
+        res.status(200).send({
+          status: 'ok',
+          message: 'Result written to file',
+          streamSid: data.streamSid
+        })
+        return;
       });
     } else {
       debug(`File ${metafile} could not be found`);
-      res.status(200).send({status: 'error', message: 'Metafile not found'})
+      res.status(200).send({
+        status: 'error',
+        message: 'Metafile not found'
+      })
       return;
     }
-  
+
   } catch (e) {
     console.log('something went wrong', e)
-    res.status(500).send({status: 'error', message: 'Error: ' + e})
+    res.status(500).send({
+      status: 'error',
+      message: 'Error: ' + e
+    })
   }
 })
 
